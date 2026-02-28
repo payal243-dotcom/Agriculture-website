6377a1ce0e75d21bf8b42255671a7ecc76a12579
Payal <payalmaheshwari796@gmail.com>
2026-02-28 01:08:25 -0800

    Initial website upload

--- index2.js
+++ index2.js
@@ -5,8 +5,15 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-const reveals = document.querySelectorAll(".reveal");
+const navItems = document.querySelectorAll("#nav-links a");
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active"); 
+  });
+});
 
+
+const reveals = document.querySelectorAll(".reveal");
 window.addEventListener("scroll", () => {
   reveals.forEach(el => {
     const elementTop = el.getBoundingClientRect().top;
@@ -16,39 +23,26 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-
 const form = document.querySelector(".contact-form");
 const topAlert = document.getElementById("topAlert");
-
 form.addEventListener("submit", function(e){
   e.preventDefault(); 
-
   topAlert.classList.add("show");
-
   setTimeout(() => {
     topAlert.classList.remove("show");
   }, 4000); 
-
   form.reset(); 
 });
-
 function initMap() {
   const location = { lat: 24.8607, lng: 67.0011 }; 
   const map = new google.maps.Map(document.getElementById("map-container"), {
     zoom: 15,
     center: location,
     styles: [ 
-      {
-        "featureType": "poi",
-        "stylers": [{ "visibility": "off" }]
-      },
-      {
-        "featureType": "transit",
-        "stylers": [{ "visibility": "off" }]
-      }
+      { "featureType": "poi", "stylers": [{ "visibility": "off" }] },
+      { "featureType": "transit", "stylers": [{ "visibility": "off" }] }
     ]
   });
-
   const marker = new google.maps.Marker({
     position: location,
     map: map,
